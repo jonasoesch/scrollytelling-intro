314e713e3330b65c61a6a52e9fb9acced38034d2
Jonas Oesch <jonas.oesch@nzz.ch>
2023-01-04 15:34:07 +0100

    Animated scroller cleanup

--- 2_animiert/js/config.js
+++ 2_animiert/js/config.js
@@ -2,33 +2,41 @@ var main = d3.select("main");
 		var scrolly = main.select("#scrolly");
 		var figure = scrolly.select("figure");
 		var article = scrolly.select("article");
-		var step = article.selectAll(".step");
 
 		// initialize the scrollama
 		var scroller = scrollama();
 
 
-		function handleStepEnter(response) {
+		let p = new p5(sketch, 'sticky'); // Neues P5-Canvas im Element mit der ID "sticky"
+		function sketch(p) {              // Konfiguration:
+			p.setup = function(){
+			  p.createCanvas(600, 600);
+			  p.noLoop()
+			}
+
+		  };
+
+
+
+		function handleStepEnter(response) { // Bei jedem neuen Step, alles löschen und mit der neuen Nummer, neu aufsetzen
 
 			p.redraw = function(position) {
 				p.clear()
-				p.background("rgba(0,0,0,0.2)")
-				p.ellipse(position, position, 20, 20)
-				p.text(response.index, position-3, position+4)
+				p.background("rgba(0,0,0,0.2)") // Den grauen Hintergrund zeichnen
+				p.ellipse(position, position, 20, 20) // Weissen Kreis darauf
+				p.text(response.index, position-3, position+4) // Text mit der Nummer des aktuellen Steps
 			}
 
-            // Little message
             console.log("Step", response.index, "entered the stage. The direction is", response.direction)
 		}
 
         function handleStepExit(response) {
-            // Little message
             console.log("Step", response.index, "exited the stage. The direction is", response.direction)
         }
 
         function handleStepProgress(response) {
             console.log("Step", response.index, ":", response.progress*100, "%")
-			p.redraw(response.progress*400)
+			p.redraw(response.progress*600) // Hier wird die Methode aufgerufen, die den Kreis neu zeichnet.
         }
 
 
@@ -56,7 +64,7 @@ var main = d3.select("main");
 			scroller
 				.setup({
 					step: "#scrolly article .step",
-					offset: 0.8,
+					offset: 0.8, // Die Textboxen sollen 0.8-Bildschirmhöhen Abstand haben
                     progress: true,
 					debug: false
 				})
@@ -68,11 +76,3 @@ var main = d3.select("main");
 		// kick things off
 		init();
 
-		function sketch(p) {
-			p.setup = function(){
-			  p.createCanvas(400, 400);
-			  p.noLoop()
-			}
-
-		  };
-		  let p = new p5(sketch, 'sticky');
